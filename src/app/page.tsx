7dc2e0303b00cd87be4ffbdcba550c4fd45571c3
LucasKoval <lucaskovaldev@gmail.com>
2024-02-29 15:22:15 -0300

    Text improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-6 sm:p-20">
          <div className="flex-col z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
             <div>
-               <h1 className="flex w-full justify-center  text-3xl font-serif pb-6 pt-6 sm:pt-0">
+               <h1 className="flex w-full justify-center text-4xl text-center font-serif pb-6 pt-6 sm:pt-0">
                   Nuestros Padres <br className="block sm:hidden" /> Deben Morir
                </h1>
             </div>
 
             <div>
-               <p className="flex w-full justify-center p-2 text-base font-sans">
+               <p className="flex w-full justify-center p-2 text-lg font-sans">
                   Apertura: 1/4/2024
                </p>
             </div>
