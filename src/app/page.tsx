25aec51f750edd51870298a37c2ca02898f83d66
LucasKoval <lucaskovaldev@gmail.com>
2024-02-29 11:05:50 -0300

    Text updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
    return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
          <div className="flex-col z-10 max-w-5xl w-full items-center justify-center font-mono text-sm lg:flex">
-            <h1 className="flex w-full justify-center">NPDM</h1>
-            <p className="flex w-full justify-center">APERTURA: 1/4</p>
+            <h1 className="flex w-full justify-center p-8 pt-0">NPDM</h1>
+            <p className="flex w-full justify-center">Apertura: 1/4/2024</p>
          </div>
       </main>
    );
