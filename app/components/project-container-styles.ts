12e331e07533eb195e69acbfcdc007efaf226a56
Lucas Koval <lucaskovaldev@gmail.com>
2024-04-02 14:55:53 -0300

    First post added

---
+++ app/components/project-container-styles.ts
@@ -0,0 +1,25 @@
+'use client';
+
+import styled from 'styled-components';
+
+export const Section = styled.section``;
+
+export const ParagraphContainer = styled.div``;
+
+export const TitleContainer = styled.div``;
+
+export const PhraseContainer = styled.div``;
+
+export const Phrase = styled.p``;
+
+export const Author = styled.p``;
+
+export const MainTitle = styled.h1``;
+
+export const H2 = styled.h2``;
+
+export const H3 = styled.h3``;
+
+export const H4 = styled.h4``;
+
+export const H5 = styled.h5``;
