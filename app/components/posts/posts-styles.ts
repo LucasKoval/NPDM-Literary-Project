1942b98a8b5f5e9239d633eaaa1e6def1f7a6f37
Lucas Koval <lucaskovaldev@gmail.com>
2024-04-06 00:54:26 -0300

    Mobile view fixed

--- app/components/posts/posts-styles.ts
+++ app/components/posts/posts-styles.ts
@@ -1,19 +1,24 @@
 'use client';
 
 import styled from 'styled-components';
+import { device } from '@/app/lib/breakpoints';
 
 export const PostContainer = styled.section`
    display: flex;
    flex-direction: column;
    justify-content: center;
    align-items: center;
-   width: 58%;
+   max-width: 550px;
+   width: 100%;
+
+   @media only screen and ${device.sm} {
+   }
 `;
 
 export const PostSection = styled.article`
    display: flex;
    flex-direction: column;
-   margin-bottom: 25rem;
+   width: 100%;
 `;
 
 export const ParagraphContainer = styled.div`
@@ -35,6 +40,7 @@ export const TitleContainer = styled.div`
    flex-direction: column;
    justify-content: center;
    align-items: center;
+   text-align: center;
 `;
 
 export const PhraseContainer = styled.div`
@@ -49,7 +55,7 @@ export const Phrase = styled.p`
 export const Author = styled.p`
    font-family: 'Times New Roman', Times, serif;
    font-size: 20px;
-   margin-top: 2rem;
+   margin-top: 1.5rem;
 `;
 
 export const MainTitle = styled.h1`
@@ -57,8 +63,6 @@ export const MainTitle = styled.h1`
 
    &.main-title-1 {
       font-size: 83px;
-      margin-top: 5rem;
-      padding-bottom: 1rem;
    }
    &.main-title-2 {
       font-size: 114px;
@@ -73,33 +77,68 @@ export const MainTitle = styled.h1`
 
 export const H2 = styled.h2`
    font-family: 'Impact', sans-serif;
-   font-size: 130px;
-   margin-top: 10rem;
-   line-height: 9.5rem;
+   font-size: 100px;
+
+   @media only screen and ${device.sm} {
+      font-size: 50px;
+   }
 `;
 
 export const H3 = styled.h3`
    font-family: 'Impact', sans-serif;
    font-size: 70px;
-   margin-top: 2rem;
-   line-height: 2rem;
+
+   @media only screen and ${device.sm} {
+      font-size: 44px;
+   }
 `;
 
 export const H4 = styled.h4`
    font-family: 'Impact', sans-serif;
    font-size: 50px;
-   margin-top: 2rem;
-   line-height: 4rem;
-   margin-bottom: -1rem;
 `;
 
 export const H5 = styled.h5`
    font-family: 'Impact', sans-serif;
    font-size: 30px;
-   margin-bottom: 2rem;
+
+   @media only screen and ${device.sm} {
+      font-size: 26px;
+   }
 `;
 
 export const Paragraph = styled.p`
    font-size: 16px;
    line-height: 2rem;
+   text-align: justify;
+
+   &.on-center {
+      text-align: center;
+   }
+`;
+
+export const Separator = styled.br`
+   content: 'separator';
+   visibility: hidden;
+   margin: 5rem 0;
+
+   &.small {
+      margin: 1rem 0;
+   }
+
+   @media only screen and ${device.sm} {
+      margin: 3rem 0;
+   }
+`;
+
+export const Break = styled.br`
+   &.mobile {
+      display: none;
+   }
+
+   @media only screen and ${device.sm} {
+      &.mobile {
+         display: block;
+      }
+   }
 `;
