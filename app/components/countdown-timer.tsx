05a095c919e3514c27cbcab468e3447ee35cb0ce
Lucas Koval <lucaskovaldev@gmail.com>
2024-04-28 19:23:45 -0300

    Timer changed

--- app/components/countdown-timer.tsx
+++ app/components/countdown-timer.tsx
@@ -5,13 +5,23 @@ import { CountdownSection, CountdownTitle } from './countdown-timer-styles';
 import { Break } from './posts/posts-styles';
 
 const CountdownTimer: React.FC = () => {
+   const currentDate = new Date();
+   const newPostDate = new Date('2024-05-01T16:00:00');
+   const nextPostDate = new Date('2024-05-16T22:00:00');
    const targetDate = new Date('2024-05-01T16:00:00').getTime();
    const [timeLeft, setTimeLeft] = useState(targetDate - Date.now());
+   let showNewPost = false;
+
+   if (currentDate >= newPostDate) {
+      showNewPost = true;
+   }
 
    useEffect(() => {
       const interval = setInterval(() => {
          const newTimeLeft = targetDate - Date.now();
          setTimeLeft(newTimeLeft);
+
+         /* Sigo aca. Logica ára que muestre el otro timer del nextPostDate */
 
          if (newTimeLeft <= 0) {
             clearInterval(interval);
@@ -34,10 +44,17 @@ const CountdownTimer: React.FC = () => {
 
    return (
       <CountdownSection>
-         <CountdownTitle>
-            PILOTO y OUT <Break className="mobile" />
-            se libera en <Break className="mobile" /> {formatTime(timeLeft)}
-         </CountdownTitle>
+         {!showNewPost ? (
+            <CountdownTitle>
+               PILOTO y OUT <Break className="mobile" />
+               se libera en <Break className="mobile" /> {formatTime(timeLeft)}
+            </CountdownTitle>
+         ) : (
+            <CountdownTitle>
+               EGOL <Break className="mobile" />
+               se libera en <Break className="mobile" /> {formatTime(timeLeft)}
+            </CountdownTitle>
+         )}
       </CountdownSection>
    );
 };
