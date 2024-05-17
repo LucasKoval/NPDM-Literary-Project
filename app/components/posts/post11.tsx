5f4efa29994bbb8ae1766353854c08a650b2637a
LucasKoval <lucaskovaldev@gmail.com>
2024-05-17 10:09:57 -0300

    Post 11 added

--- app/components/posts/post11.tsx
+++ app/components/posts/post11.tsx
@@ -20,7 +20,7 @@ export default function Post11() {
       <>
          <PostSection id="page-13">
             <TitleContainer>
-               <H4 className="big">XXX</H4>
+               <H4 className="big">37-38°C</H4>
             </TitleContainer>
 
             <Separator />
@@ -29,9 +29,10 @@ export default function Post11() {
 
             <PhraseContainer>
                <Phrase>
-                  <Break />
+                  Lord Almighty
+                  <Break />I feel my temperature rising
                </Phrase>
-               <Author>-XXX</Author>
+               <Author>-BURNING LOVE & HITS FROM HIS MOVIES, VOL. 2</Author>
             </PhraseContainer>
 
             <Separator />
@@ -39,8 +40,104 @@ export default function Post11() {
             <Separator className="mobile" />
 
             <ParagraphContainer>
-               <Paragraph className="align-center">
+               <Paragraph className="align-justify">
+                  en este mismo sofá, me hice mi primera paja{' '}
+                  <span className="large spacing-s">
+                     nochextraña y sacraceitosa
+                  </span>
                   <Break />
+                  mi primo Pacho me enseñó cómo{' '}
+                  <span className="large spacing-m">
+                     confirma Sión al frente olivalsámico
+                  </span>
+                  <Break />
+                  teníamos once años y toda la quinta para nosotros{' '}
+                  <span className="large">padrino mentrega papá</span>
+                  <Break />
+                  él en aquel extremo y yo en este (ya no es tan largo el sofá)
+                  <span className="large">1 nervio sismo</span>
+                  <Break />
+                  peló su pilín y comenzó a ilustrarme{' '}
+                  <span className="large spacing-m">
+                     <em>alguien te quiere conocer</em> me dijo
+                  </span>
+                  <Break />
+                  (siempre tuvo manos de basquetbolista{' '}
+                  <span className="large ">
+                     caminamos en silencio un trecho
+                  </span>
+                  <Break />
+                  usaba nada más los dedos índice y pulgar){' '}
+                  <span className="large">
+                     y <em>alguien</em> bajó de la camioneta
+                  </span>
+                  <Break />
+                  era gracioso pero ahora él mantenía tal solemnidad y no me
+                  quise reír{' '}
+                  <span className="large spacing-m">
+                     <em>hola</em>
+                  </span>
+                  <Break />
+                  -probá. yo, ya que estoy, me voy a “echar” una{' '}
+                  <span className="large spacing-s">
+                     dijo y me agarró del brazo
+                  </span>
+                  <Break />
+                  así que pelé yo también y comencé a hacer según se me indicó{' '}
+                  <span className="large spacing-s">y me apartó</span>
+                  <Break />
+                  no le encontraba la gracia, pero le ponía voluntad y
+                  entusiasmo{' '}
+                  <span className="large spacing-m">de mi viejo</span>
+                  <Break />
+                  en una, sin dejar de masturbarse, él me dice{' '}
+                  <span className="large spacing-l">
+                     <em>qué gusto conocerte por fin</em>
+                  </span>
+                  <Break />
+                  -cuando “acabás” se siente bien en todo el cuerpo{' '}
+                  <span className="large spacing-s">
+                     <em>soy la mujer de tu papá</em>
+                  </span>
+                  <Break />
+                  eso me motivó; exploraba agarres, velocidades{' '}
+                  <span className="large spacing-l">
+                     ya había oído su nombre
+                  </span>
+                  <Break />
+                  excitado, una vez que le agarré la mano{' '}
+                  <span className="large spacing-s">
+                     en esas discusiones por teléfono
+                  </span>
+                  <Break />
+                  fueron unos segundos, y entonces la explosión{' '}
+                  <span className="large spacing-s">
+                     <em>pero quiero que sepas que</em>
+                  </span>
+                  <Break />
+                  nunca fue tan superada una expectativa{' '}
+                  <span className="large spacing-l">
+                     <em>yo jamás voy a ocupar el lugar</em>
+                  </span>
+                  <Break />
+                  no podía superar que eligió la palabra{' '}
+                  <em>
+                     bien... ¿¡bien!?{' '}
+                     <span className="large">de tu mamá ¿sabés?</span>
+                  </em>
+                  <Break />y menos aún que, al acabar él, se levantó casi de un
+                  salto <span className="large spacing-xl">volví con papá</span>
+                  <Break />
+                  me vio, fulminado, y no pudo ocultar el orgullo propio{' '}
+                  <span className="large spacing-s">él sonreía aliviado</span>
+                  <Break />
+                  -vamos al aro, te juego un pique veintiuno- y saliendo{' '}
+                  <span className="large">me dijo sin mirarme</span>
+                  <Break />
+                  -¡dale, no seas pajero!{' '}
+                  <span className="large">
+                     <em>y bueno, ¿ya pensaste adónde querés ir a cenar?</em>
+                  </span>
                </Paragraph>
             </ParagraphContainer>
 
